dcbb851ddd4d7a0038051fa27b2f0358ca381768
Muskaan <muskaanyadavonline@gmail.com>
2025-05-29 12:25:45 +0530

    Added Hero.jsx

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Hero</h1>
+    <div className='flex flex-col items-center justify-center w-full md:pt-36 pt-20
+    px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70'>
+      <h1 className='md:text-home-heading-large text-home-heading-small relative font-bold text-gray-
+      800 max-w-3xl mx-auto'>Empower  your future with the courses designed to <span>
+       fit your choice.</span><img src={assets.sketch}
+       alt="sketch"  className='md:block hidden absolute -bottom-7 right-0' /></h1>
+
+
+
+       <p className='md:block hidden text-gray-500 max-w-2xl mx-auto'> We bring together world-class instructors, interactive content, and a suppotive
+       community to help you acheive your personal and professional goals.</p>
+
+
+<p className='md:block hidden text-gray-500 max-w-2xl mx-auto'
+>We bring together world-class instructors to help you achieve your professional goals.</p>
+
+
     </div>
   );
 }
