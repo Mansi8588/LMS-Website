f2344e221aa14df8fa6d97242c127bc5e08ca710
Mansi8588 <ajmrhw12@gmail.com>
2025-05-30 10:37:26 +0530

    youtube video and loading page done

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -5,6 +5,8 @@ import Loading from '../../components/student/Loading';
 import { assets } from '../../assets/assets';
 import humanizeDuration from 'humanize-duration';
 import Footer from '../../components/student/Footer'
+import YouTube from 'react-youtube'
+
 
 const CourseDetails = () => {
   const { id } = useParams();
@@ -27,7 +29,7 @@ calculateChapterTime,currency} = useContext(AppContext);
 
   useEffect(() => {
     fetchCourseData();
-  }, [allCourses, id]);
+  }, [allCourses]);
 
 
 const toggleSection=(index)=>{
@@ -126,9 +128,12 @@ underline'>GreatStack</span></p>
           <div className='flex items-center justify-between w-full text-gray-800 text-xs md:text-default'>
             <p>{lecture.lectureTitle}</p>
             <div className='flex gap-2'>
-              {lecture.isPreviewFree && (
-                <p className='text-blue-500 cursor-pointer'>Preview</p>
-              )}
+              {lecture.isPreviewFree && 
+                <p onClick={()=>setPlayerData(
+                  {videoId:lecture.lectureUrl.split('/').pop() }
+                )}
+                 className='text-blue-500 cursor-pointer'>Preview</p>
+              }
               <p>
                 {humanizeDuration(lecture.lectureDuration * 60 * 1000, {
                   units: ['h', 'm'],
@@ -167,12 +172,19 @@ underline'>GreatStack</span></p>
       {/* Right column */}
       <div className='max-w-course-card z-10 shadow-custom-card rounded-t
       md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]'>
+{
+  playerData?
+<YouTube videoId={playerData.videoId} opts={{playerVars:{autoplay:1}}} iframeClassName='w-full aspect-video'/>
 
+
+ : 
+ 
 <img src={courseData.courseThumbnail} alt=""/>
-<div className='pt-5'>
-<div>
-  <img src={assets.time_left_clock_icon} alt=""/>
-
+}
+<div className='p-5'>
+<div className='flex items-center gap-2'>
+  <img className='w-3.5' src={assets.time_left_clock_icon} alt="time left clock icon"/>
+  
   <p className='text-red-500'><span className='font-medium'>5 days</
   span> left at this price!</p>
 </div>
